59c8942db508622f3b8c7cefab997e412619e14e
atulyam91-ai <atulyam91@gmail.com>
2026-09-19 10:43:24 +0530

    sum of elements of array

--- sum_elements_of_array.c
+++ sum_elements_of_array.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int arr[5];
+    int arr[5], i, sum = 0;
     printf("Enter 5 numbers:\n");
     for(int i=0; i<5; i++)
     {
         scanf("%d", &arr[i]);
     }
     
-    printf("\n\nEntered elements in an array:\n");
     for(int i=0; i<5; i++)
     {
-        printf("elements in the array:%d at location %d\n", arr[i],&arr[i]);
+        sum += arr[i];
     }
-    
+    printf("Sum of the elements in the array: %d\n", sum);
     return 0;
 }
