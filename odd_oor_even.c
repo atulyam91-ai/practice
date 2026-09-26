909ccb4c7215e9524c9b8914c24fe1c9cb0178e3
atulyam91-ai <atulyam91@gmail.com>
2026-09-26 22:38:36 +0530

    odd or even check

---
+++ odd_oor_even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("enter an integer");
+    scanf("%d",&number);
+    if(number%2==0){
+
+    
+        printf("%d is an even intger",number);
+    }
+    else{
+        printf("%d is an odd integer",number);
+    }
+
+    
+
+}
